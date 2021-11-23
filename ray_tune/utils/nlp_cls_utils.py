1623190281e951a2507f0e8ff83382ac459cfbb3
fanlai <fanlai@h1.keeper-nni.gaia-pg0.wisc.cloudlab.us>
2021-11-23 03:00:55 -0600

    t

--- ray_tune/utils/nlp_cls_utils.py
+++ ray_tune/utils/nlp_cls_utils.py
@@ -10,17 +10,24 @@ import inspect
 from transformers import TrainingArguments, get_linear_schedule_with_warmup
 from torch.utils.data import DataLoader
 import logging
+from torch.nn.utils.rnn import pad_sequence
 
 total_steps = 20
+
+def collate(examples, pid):
+    #if tokenizer._pad_token is None:
+    #    return pad_sequence(examples, batch_first=True)
+    return pad_sequence(examples, batch_first=True, padding_value=pid)
 
 def eval_nlp_cls(model, test_loader, device=torch.device("cuda")):
 
     def compute_metrics(logits, labels):
         predictions = torch.argmax(logits, axis=-1)
-        return torch.sum(predictions == labels)/len(labels)
+        return torch.sum(predictions == labels)
         #return metric.compute(predictions=predictions, references=labels)
 
     model.eval()
+    model = model.to(device=device)
     total_acc = total_loss = 0.
     step = 0
     for inputs in test_loader:
@@ -32,8 +39,8 @@ def eval_nlp_cls(model, test_loader, device=torch.device("cuda")):
         step += 1
         if step > total_steps:
             break
-    # logging.info(f"Eval loss: {total_loss/len(test_loader)}, accuracy: {total_acc*100./len(test_loader)}")
-    return total_acc/len(test_loader), total_loss/len(test_loader)
+    logging.info(f"Eval loss: {total_loss/len(test_loader)}, accuracy: {total_acc*100./len(test_loader.dataset)}")
+    return total_acc/len(test_loader.dataset), total_loss/len(test_loader)
 
 
 def train_nlp_cls(model, tokenizer, train_loader, optimizer, device=torch.device("cuda"), scheduler=None):
@@ -62,47 +69,51 @@ def train_nlp_cls(model, tokenizer, train_loader, optimizer, device=torch.device
 def load_cls_model(name, num_labels=5):
     max_text_length = 256
     tokenizer = AutoTokenizer.from_pretrained(name, model_max_length=max_text_length)
+    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
     config = AutoConfig.from_pretrained(name)
     config.num_labels = num_labels
 
     model = AutoModelForSequenceClassification.from_config(config)
-
-    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    model.resize_token_embeddings(len(tokenizer))
+    
     model.config.max_length = max_text_length
 
     return model, tokenizer
 
 
 def main():
-    device = 'cpu'
+    device = 'cuda'
     model, tokenizer = load_cls_model("albert-base-v2")
     train_dataset = load_dataset("yelp_review_full", split="train")
     test_dataset = load_dataset("yelp_review_full", split="test")
     train_dataset = train_dataset.rename_column('label', 'labels')
     test_dataset = test_dataset.rename_column('label', 'labels')
 
-    train_dataset = train_dataset.map(lambda batch: tokenizer(batch["text"], truncation=True, padding=True), batched=True)
+    #train_dataset = train_dataset.map(lambda batch: tokenizer(batch["text"], truncation=True, padding=True), batched=True)
     test_dataset = test_dataset.map(lambda batch: tokenizer(batch["text"], truncation=True, padding=True), batched=True)
 
-    print(train_dataset.keys())
-    return
-    train_dataset.set_format(type='torch', columns=['attention_mask', 'input_ids', 'token_type_ids', 'labels'])
-    test_dataset.set_format(type='torch', columns=['attention_mask', 'input_ids', 'token_type_ids', 'labels'])
+    interest_args = [x for x in inspect.getargspec(model.forward).args if x != 'self']
+    interested_args = [x for x in list(test_dataset.features.keys()) if x in interest_args]
+    #print(interested_args)
+    #train_dataset.set_format(type='torch', columns=interested_args)
+    test_dataset.set_format(type='torch', columns=interested_args)
 
 
-    batch_size = 12
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+    batch_size = 16
+    
+    #train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=4)#, 
+            #collate_fn=lambda b: collate(b, tokenizer.pad_token_id))
 
-    WARMUP_STEPS = int(0.2*len(train_loader))
+    WARMUP_STEPS = 20#int(0.2*len(train_loader))
     EPOCHS = 30
     optimizer = torch.optim.AdamW(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-5, eps=1e-8)
     scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=WARMUP_STEPS,
-                                    num_training_steps=len(train_loader)*EPOCHS)
+                                    num_training_steps=12)#len(train_loader)*EPOCHS)
     #scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=4, verbose=True, min_lr=0, factor=0.2)
 
     model = model.to(device=device)
-    train_nlp_cls(model, tokenizer, train_loader, optimizer, device, scheduler)
+    train_nlp_cls(model, tokenizer, test_loader, optimizer, device, scheduler)
     eval_nlp_cls(model, test_loader)
 
 #main()
